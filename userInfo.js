6ee9840d9aeda88c7b5a6d6c21491ce4feb0f578
cruz-ramirez <90863541+cruz-ramirez@users.noreply.github.com>
2024-05-19 22:50:15 +0000

    Add files via upload

---
+++ userInfo.js
@@ -0,0 +1,9 @@
+
+// Task 3
+function createUserProfiles(names, modifiedNames) {
+    return names.map((name, index) => ({
+        originalName: name,
+        modifiedName: modifiedNames[index],
+        id: index + 1
+    }));
+}
